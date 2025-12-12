ea1975396f03412eb871b72eecfa3b5d5c24961a
creathydev <creathy23@gmail.com>
2025-12-12 22:16:51 +0530

    Google tag

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -54,4 +54,15 @@ const Index = () => {
   );
 };
 
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-78V05BQZT3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-78V05BQZT3');
+</script>
+
 export default Index;
